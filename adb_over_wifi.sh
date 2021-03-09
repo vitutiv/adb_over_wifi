9769b775e602dbc2c0e22c748c2187c58e31c454
Victor Pereira <37667229+vitutiv@users.noreply.github.com>
2021-03-08 22:46:05 -0300

    added scrcpy support
    Now you can use this script to automate phone display mirroring after connection.
    Warning: for this to work, you need to use '-s' as an argument, and have scrcpy installed in your computer.

--- adb_over_wifi.sh
+++ adb_over_wifi.sh
@@ -4,15 +4,19 @@ adb kill-server
 #echo 'Staring Server'
 adb start-server
 #echo 'Showing devices'
-adb devices
+#adb devices
 #echo 'Getting ip address'
 ip_address=`adb shell ip addr show wlan0 | grep 'inet ' | cut -d' ' -f6 | cut -d/ -f1`
 #echo 'IP Address: '$ip_address
 if [[ ! -z $ip_address ]]; then
 	#echo 'Connecting over TCP on port 5555...'
 	adb tcpip 5555
-    #echo "Connecting to $ip_address..."
-    adb connect $ip_address
+	#echo 'Connecting to $ip_address...'
+	adb connect $ip_address
+	if [[ $1 == '-s' ]]; then
+		#echo 'Displaying phone screen...'
+		scrcpy
+	fi
 else
     echo 'No ip address detected. '
 fi
